a58e7981ff56f35b2e5a313e9be6186fa014eac8
Betaproject18 <jmifflin18@gmail.com>
2022-05-31 10:54:52 -0400

    Added Python Prework Q's

---
+++ Python Prework Questions/Python Questions.py
@@ -0,0 +1,7 @@
+# Question 1
+#Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+def Hello_name(user_name):
+    name = user_name(input())
+    
+
+    Hello_name()
